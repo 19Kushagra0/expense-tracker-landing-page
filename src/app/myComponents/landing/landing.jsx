954f7a2721de5cda4aac3c2038b94ee7955cf28b
19Kushagra0 <19Kushagra0@gmail.com>
2025-11-10 17:43:07 +0530

    nextJs setup created

---
+++ src/app/myComponents/landing/landing.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "@/app/myComponents/landing/landing.css";
+
+export default function landing() {
+  return <div className="landing-page"></div>;
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import LandingPage from "@/app/myComponents/landing/landing";
+
+export default function Home() {
+  return (
+    <div className="app">
+      <LandingPage />
+    </div>
+  );
+}
