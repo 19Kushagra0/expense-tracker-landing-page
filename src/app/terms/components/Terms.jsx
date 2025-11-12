c499d20b403e21fd26d22f2818967cc6bdf5d7aa
19Kushagra0 <19Kushagra0@gmail.com>
2025-11-12 17:06:28 +0530

    navbar created (#5)

--- src/app/terms/components/Terms.jsx
+++ src/app/terms/components/Terms.jsx
@@ -1,87 +1,158 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import "@/app/style.css";
 import Link from "next/link";
+import { Inter } from "next/font/google";
+import Navbar from "@/app/myComponents/Navbar/Navbar";
 export default function Terms() {
+  const [navBar, setNavBar] = useState(false);
+
+  const showNavBar = () => {
+    setNavBar(!navBar);
+  };
   return (
     <div className="termsPage">
+      <div className="term-privacy-header">
+        <Link href="/">
+          <svg
+            className="term-privacy-header-image-container"
+            height={36}
+            width={36}
+            viewBox="0 0 35 38"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M13.2188 1.14714C15.868 -0.38238 19.132 -0.38238 21.7812 1.14714L30.7188 6.30733C33.368 7.83685 35 10.6635 35 13.7226V24.0428C35 27.1018 33.368 29.9285 30.7188 31.4581L21.7812 36.6182C19.6801 37.8313 17.1924 38.082 14.9313 37.3709V18.3261C14.9313 14.9923 12.2286 12.2896 8.89478 12.2896C5.56092 12.2896 2.85829 14.9923 2.85829 18.3261V30.4284C1.06016 28.8223 0 26.5081 0 24.0428V13.7226C0 10.6635 1.63199 7.83685 4.2812 6.30733L13.2188 1.14714Z"
+              fill="black"
+            />
+          </svg>
+        </Link>
+        <div className="term-privacy-header-button-container">
+          <Link href="/" className="term-privacy-header-button">
+            Carrers
+          </Link>
+          <Link href="/privacy" className="term-privacy-header-button">
+            Privacy
+          </Link>
+          <Link href="/terms" className="black term-privacy-header-button">
+            Terms
+          </Link>
+        </div>
+        <button
+          onClick={() => {
+            showNavBar();
+          }}
+          className="term-privacy-header-hamberburger-container"
+        >
+          <svg
+            width="29"
+            height="29"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="term-privacy-header-hamberburger"
+          >
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M2 6C2 5.44772 2.44772 5 3 5H21C21.5523 5 22 5.44772 22 6C22 6.55228 21.5523 7 21 7H3C2.44772 7 2 6.55228 2 6ZM2 12C2 11.4477 2.44772 11 3 11H21C21.5523 11 22 11.4477 22 12C22 12.5523 21.5523 13 21 13H3C2.44772 13 2 12.5523 2 12ZM2 18C2 17.4477 2.44772 17 3 17H21C21.5523 17 22 17.4477 22 18C22 18.5523 21.5523 19 21 19H3C2.44772 19 2 18.5523 2 18Z"
+              fill="#C0C0C0"
+            />
+          </svg>
+        </button>
+      </div>
+      {navBar ? <Navbar showNavBar={showNavBar} /> : null}
       {/* Title */}
-      <div className="title-container">
-        <div className="title-box">
-          <span className="title">Terms of Service</span>
-          <div className="time-container">
-            <span className="last-update">Last updated</span>
-            <span className="time">12th Sept, 2025</span>
+
+      <div className="terms-privacy-title-container">
+        <div className="terms-privacy-title-box">
+          <span className="terms-privacy-title">Terms of Service</span>
+          <div className="terms-privacy-time-container">
+            <span className="terms-privacy-last-update">Last updated</span>
+            <span className="terms-privacy-time">12th Sept, 2025</span>
           </div>
         </div>
       </div>
 
-      <span className="content-intro2">
+      <span className="terms-privacy-content-intro2">
         Numora ("we", "our", or "us") is committed to protecting your privacy.
         This Privacy Policy explains how we collect, use, and protect
         information when you use the Numora mobile application and related
         services (collectively, the "App"). Numora is a free personal and split
         expense management app.
       </span>
-
       {/* Content and index */}
-      <div className="index-content-container">
-        {/* Content */}
-        <div className="index-container">
-          <button className="index">
-            <span className="index-number">1.</span>
-            <span className="index-name">Using Numora</span>
+      <div className="terms-privacy-index-content-container">
+        {/* Index */}
+        <div className="terms-privacy-index-container">
+          <button className="terms-privacy-index">
+            <span className="terms-privacy-index-number">1.</span>
+            <span className="terms-privacy-index-name">Using Numora</span>
           </button>
-          <button className="index">
-            <span className="index-number black">2.</span>
-            <span className="index-name black">Account & Authentication</span>
+          <button className="terms-privacy-index">
+            <span className="terms-privacy-index-number black">2.</span>
+            <span className="terms-privacy-index-name black">
+              Account & Authentication
+            </span>
           </button>
-          <button className="index">
-            <span className="index-number">3.</span>
-            <span className="index-name">User Content</span>
+          <button className="terms-privacy-index">
+            <span className="terms-privacy-index-number">3.</span>
+            <span className="terms-privacy-index-name">User Content</span>
           </button>
-          <button className="index">
-            <span className="index-number">4.</span>
-            <span className="index-name">Prohibited Conduct</span>
+          <button className="terms-privacy-index">
+            <span className="terms-privacy-index-number">4.</span>
+            <span className="terms-privacy-index-name">Prohibited Conduct</span>
           </button>
-          <button className="index">
-            {" "}
-            <span className="index-number">5.</span>
-            <span className="index-name">Disclaimer of Warranties</span>
+          <button className="terms-privacy-index">
+            <span className="terms-privacy-index-number">5.</span>
+            <span className="terms-privacy-index-name">
+              Disclaimer of Warranties
+            </span>
           </button>
-          <button className="index">
-            {" "}
-            <span className="index-number">6.</span>
-            <span className="index-name">Limitations of Liability</span>
+          <button className="terms-privacy-index">
+            <span className="terms-privacy-index-number">6.</span>
+            <span className="terms-privacy-index-name">
+              Limitations of Liability
+            </span>
           </button>
-          <button className="index">
-            <span className="index-number">7.</span>
-            <span className="index-name">Account Termination</span>
+          <button className="terms-privacy-index">
+            <span className="terms-privacy-index-number">7.</span>
+            <span className="terms-privacy-index-name">
+              Account Termination
+            </span>
           </button>
-          <button className="index">
-            <span className="index-number">8.</span>
-            <span className="index-name">Changes to the Terms</span>
+          <button className="terms-privacy-index">
+            <span className="terms-privacy-index-number">8.</span>
+            <span className="terms-privacy-index-name">
+              Changes to the Terms
+            </span>
           </button>
-          <button className="index">
-            <span className="index-number">9.</span>
-            <span className="index-name">Governing Law</span>
+          <button className="terms-privacy-index">
+            <span className="terms-privacy-index-number">9.</span>
+            <span className="terms-privacy-index-name">Governing Law</span>
           </button>
-          <button className="index">
-            <span className="index-number">10.</span>
-            <span className="index-name">Contact Us</span>
+          <button className="terms-privacy-index">
+            <span className="terms-privacy-index-number">10.</span>
+            <span className="terms-privacy-index-name">Contact Us</span>
           </button>
         </div>
-        {/* Index */}
-        <div className="content-conatiner">
-          <span className="content-intro">
+
+        {/* Content */}
+        {/* Content */}
+        <div className="terms-privacy-content-container">
+          <span className="terms-privacy-content-intro">
             These Terms of Service ("Terms") govern your use of the Numora
             mobile application and related services (the "App"). By accessing or
             using Numora, you agree to be bound by these Terms. If you do not
             agree, do not use the App.
           </span>
-          <div className="content-title-text-container">
-            <div className="content-title-text-box">
-              <span className="content-title">1. Using Numora</span>
-              <span className="content-text">
+          <div className="terms-privacy-content-title-text-container">
+            <div className="terms-privacy-content-title-text-box">
+              <span className="terms-privacy-content-title">
+                1. Using Numora
+              </span>
+              <span className="terms-privacy-content-text">
                 Numora is a free personal and split expense management app. You
                 must be at least 13 years old to use the App. You are
                 responsible for keeping your account credentials secure. We are
@@ -89,29 +160,35 @@ export default function Terms() {
                 to your account.
               </span>
             </div>
-            <div className="content-title-text-box">
-              <span className="content-title">2. Account & Authentication</span>
-              <span className="content-text">
+            <div className="terms-privacy-content-title-text-box">
+              <span className="terms-privacy-content-title">
+                2. Account & Authentication
+              </span>
+              <span className="terms-privacy-content-text">
                 Numora uses Google Sign-In via Firebase Authentication. By
                 signing in, you authorize us to access basic profile information
                 (name, email, profile picture) provided by Google for
                 authentication purposes.
               </span>
             </div>
-            <div className="content-title-text-box">
-              <span className="content-title">3.  User Content</span>
-              <span className="content-text">
+            <div className="terms-privacy-content-title-text-box">
+              <span className="terms-privacy-content-title">
+                3. User Content
+              </span>
+              <span className="terms-privacy-content-text">
                 You are solely responsible for the expense entries and other
                 content you add to Numora. You warrant that you have the right
                 to submit such content and that it does not violate any
                 applicable laws or the rights of third parties.
               </span>
             </div>
-            <div className="content-title-text-box">
-              <span className="content-title">4. Prohibited Conduct</span>
-              <span className="content-text gap-10px">
+            <div className="terms-privacy-content-title-text-box">
+              <span className="terms-privacy-content-title">
+                4. Prohibited Conduct
+              </span>
+              <span className="terms-privacy-content-text terms-privacy-gap-10px">
                 When using Numora you agree not to:
-                <ul className="highlight custom-list ">
+                <ul className="terms-privacy-highlight terms-privacy-custom-list">
                   <li>
                     Upload content that is illegal, harmful, or infringes
                     third-party rights.
@@ -126,71 +203,86 @@ export default function Terms() {
                 </ul>
               </span>
             </div>
-            <div className="content-title-text-box">
-              <span className="content-title">5. Disclaimer of Warranties</span>
-              <span className="content-text">
+            <div className="terms-privacy-content-title-text-box">
+              <span className="terms-privacy-content-title">
+                5. Disclaimer of Warranties
+              </span>
+              <span className="terms-privacy-content-text">
                 Numora is provided "as is" and "as available". We do not
                 guarantee the App will be error-free or uninterrupted. To the
                 fullest extent permitted by law, we disclaim all warranties,
                 express or implied.
               </span>
             </div>
-            <div className="content-title-text-box">
-              <span className="content-title">6. Limitation of Liability</span>
-              <span className="content-text">
+            <div className="terms-privacy-content-title-text-box">
+              <span className="terms-privacy-content-title">
+                6. Limitation of Liability
+              </span>
+              <span className="terms-privacy-content-text">
                 To the maximum extent permitted by law, Numora and its
                 developers will not be liable for any indirect, incidental,
                 special, consequential, or punitive damages arising out of your
                 use of the App.
               </span>
             </div>
-            <div className="content-title-text-box">
-              <span className="content-title">7. Account Termination</span>
-              <span className="content-text">
+            <div className="terms-privacy-content-title-text-box">
+              <span className="terms-privacy-content-title">
+                7. Account Termination
+              </span>
+              <span className="terms-privacy-content-text">
                 We may suspend or terminate your access to Numora at our
                 discretion for violations of these Terms or for any conduct that
                 we determine is harmful to other users. You may also delete your
                 account at any time by contacting us.
               </span>
             </div>
-            <div className="content-title-text-box">
-              <span className="content-title">8. Changes to the Terms</span>
-              <span className="content-text">
+            <div className="terms-privacy-content-title-text-box">
+              <span className="terms-privacy-content-title">
+                8. Changes to the Terms
+              </span>
+              <span className="terms-privacy-content-text">
                 We may update these Terms occasionally. If we make material
                 changes we will update the "Last updated" date above. Continued
                 use of the App after changes implies acceptance of the updated
                 Terms.
               </span>
             </div>
-            <div className="content-title-text-box">
-              <span className="content-title">9. Governing Law</span>
-              <span className="content-text">
+            <div className="terms-privacy-content-title-text-box">
+              <span className="terms-privacy-content-title">
+                9. Governing Law
+              </span>
+              <span className="terms-privacy-content-text">
                 These Terms are governed by and construed in accordance with the
                 laws of the jurisdiction where Numora is operated, without
                 regard to conflict of law provisions.
               </span>
             </div>
-            <div className="content-title-text-box">
-              <span className="content-title">10. Contact Us</span>
-              <span className="content-text">
+            <div className="terms-privacy-content-title-text-box">
+              <span className="terms-privacy-content-title">
+                10. Contact Us
+              </span>
+              <span className="terms-privacy-content-text">
                 If you have questions about these Terms or wish to delete your
                 account, contact us at:
-                <span className="highlight text-decoration">
+                <a
+                  href="mailto:contact@getnumora.com"
+                  className="terms-privacy-highlight terms-privacy-text-decoration"
+                >
                   contact@getnumora.com
-                </span>
+                </a>
               </span>
             </div>
           </div>
         </div>
       </div>
       {/* Border */}
-      <div className="line-conatiner">
-        <div className="line"></div>
+      <div className="terms-privacy-line-conatiner">
+        <div className="terms-privacy-line"></div>
       </div>
       {/* Footer */}
-      <div className="footer">
+      <div className="terms-privacy-footer">
         <svg
-          className="numora-icon"
+          className="terms-privacy-numora-icon"
           width="35"
           height="38"
           viewBox="0 0 35 38"
@@ -202,16 +294,26 @@ export default function Terms() {
             fill="black"
           />
         </svg>
-        <div className="footer-button-container">
-          <button className="footer-button-text"> Careers</button>
+        <div className="terms-privacy-footer-button-container">
+          <Link href="/" className="terms-privacy-footer-button-text">
+            Careers
+          </Link>
 
-          <Link href="/privacy" className="footer-button-text">
+          <Link
+            href="/privacy"
+            className="terms-privacy-footer-button-text terms-privacy-privacy"
+          >
             Privacy
           </Link>
-          <Link href="/terms" className="footer-button-text black">
+          <Link
+            href="/terms"
+            className="terms-privacy-footer-button-text terms-privacy-terms terms-privacy-black"
+          >
             Terms
           </Link>
-          <span className="footer-button-text">© 2025 Numora</span>
+          <span className="terms-privacy-footer-button-text">
+            © 2025 Numora
+          </span>
         </div>
       </div>
     </div>
