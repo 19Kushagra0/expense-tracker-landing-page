5d669e3ee67f19c33ef98318b80081f278c8e329
19Kushagra0 <19Kushagra0@gmail.com>
2025-11-13 03:57:35 +0530

    trying OG image

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,14 @@
 import Image from "next/image";
 import LandingPage from "@/app/myComponents/landing/landing";
-
+export const metadata = {
+  title: "Home | My Website",
+  description: "Welcome to my awesome homepage.",
+  openGraph: {
+    title: "Home | My Website",
+    description: "Welcome to my awesome homepage.",
+    images: ["/images/og-landing.png"],
+  },
+};
 export default function Home() {
   return (
     <div className="app">
