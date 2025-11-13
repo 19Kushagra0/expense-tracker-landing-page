ec604963a0f2ab740c84c9ad1fe8adbf5fbf44b1
19Kushagra0 <19Kushagra0@gmail.com>
2025-11-13 14:18:07 +0530

    Dev kushagra (#19)
    * navbar created
    
    * navbar added to landing page
    
    * Careers spelling mistake solved
    
    * style.css unnecessary comments and duplicate class definitions removed
    
    * style.css edited and auto scroll added
    
    * navbar design changed
    
    * demo career page created
    
    * career page connected and final bugs solved
    
    * created auto scrolling in privacy page
    
    * navbar edited
    
    changed navbar dark mode to light mode and box shadow removed
    
    * navbar edited
    
    * trying OG image
    
    * og image height and weight updated
    
    * dynamic og image testing

---
+++ src/app/api/og/LandingOg.jsx
@@ -0,0 +1,51 @@
+// app/components/og/LandingOg.jsx
+export default function LandingOg() {
+  return (
+    <div
+      style={{
+        width: "1200px",
+        height: "630px",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: "40px",
+        backgroundColor: "#ebebeb",
+        fontFamily: "Inter, sans-serif",
+      }}
+    >
+      {/* LEFT SECTION */}
+      <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
+        <span
+          style={{
+            fontSize: "96px",
+            fontWeight: 600,
+            color: "rgba(0,0,0,0.1)",
+          }}
+        >
+          SPEND
+        </span>
+        <span style={{ fontSize: "96px", fontWeight: 600, color: "#000" }}>
+          TRACK
+        </span>
+        <span
+          style={{
+            fontSize: "96px",
+            fontWeight: 600,
+            color: "rgba(0,0,0,0.1)",
+          }}
+        >
+          SPLIT
+        </span>
+      </div>
+
+      {/* RIGHT SECTION */}
+      <img
+        src="https://yourdomain.com/images/hero-banner.png"
+        alt="Hero Banner"
+        width={377}
+        height={815}
+        style={{ objectFit: "contain" }}
+      />
+    </div>
+  );
+}
