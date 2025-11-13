3dba148887ef4f304c3eba0cb6241ab4d6630dcd
19Kushagra0 <19Kushagra0@gmail.com>
2025-11-13 20:14:00 +0530

    Dev kushagra (#32)
    * navbar created
    
    * navbar added to landing page
    
    * Careers spelling mistake solved
    
    * style.css unnecessary comments and duplicate class definitions removed
    
    * style.css edited and auto scroll added
    
    * navbar design changed
    
    * demo career page created
    
    * career page connected and final bugs solved
    
    * created auto scrolling in privacy page
    
    * navbar edited
    
    changed navbar dark mode to light mode and box shadow removed
    
    * navbar edited
    
    * trying OG image
    
    * og image height and weight updated
    
    * dynamic og image testing
    
    * og image test 2
    
    * og location fixed
    
    * og image location changed
    
    * removing edge
    
    * Og test 3
    
    * og test 4
    
    * landPageOg edited
    
    * using image in dynamic og
    
    * bug fixed
    
    * dynamic og image edited
    
    * og image color darken
    
    * folder structure change for og image
    
    * term og page created

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,13 +2,12 @@ import LandingPage from "@/app/myComponents/landing/landing";
 
 export const metadata = {
   title: "Home | My Website",
-  description: "Welcome to my awesome homepage.",
+  description: "Welcome to my  homepage.",
   openGraph: {
     title: "Home | My Website",
-    description: "Welcome to my awesome homepage.",
+    description: "Welcome to my  homepage.",
     images: [
       {
-        // 👇 updated to match your new OG route
         url: "https://expense-tracker-alpha-lyart.vercel.app/api/og/landingOg",
         width: 1200,
         height: 630,
