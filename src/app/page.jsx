c816ecc3a83ed715031e8e67ff489bc7c24fe384
19Kushagra0 <19Kushagra0@gmail.com>
2025-11-14 00:01:21 +0530

    Dev kushagra (#38)
    * navbar created
    
    * navbar added to landing page
    
    * Careers spelling mistake solved
    
    * style.css unnecessary comments and duplicate class definitions removed
    
    * style.css edited and auto scroll added
    
    * navbar design changed
    
    * demo career page created
    
    * career page connected and final bugs solved
    
    * created auto scrolling in privacy page
    
    * navbar edited
    
    changed navbar dark mode to light mode and box shadow removed
    
    * navbar edited
    
    * trying OG image
    
    * og image height and weight updated
    
    * dynamic og image testing
    
    * og image test 2
    
    * og location fixed
    
    * og image location changed
    
    * removing edge
    
    * Og test 3
    
    * og test 4
    
    * landPageOg edited
    
    * using image in dynamic og
    
    * bug fixed
    
    * dynamic og image edited
    
    * og image color darken
    
    * folder structure change for og image
    
    * term og page created
    
    * craeted privacy og image
    
    * trying to create differet link using og images
    
    * edited link in og/route
    
    * folder structure changed
    
    * copy page mistake solved
    
    * og change again

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,22 +1,28 @@
 import LandingPage from "@/app/myComponents/landing/landing";
 
-export const metadata = {
-  title: "Home | My Website",
-  description: "Welcome to my homepage.",
-  openGraph: {
+export const dynamic = "force-dynamic"; // ⭐ REQUIRED ⭐
+
+export function generateMetadata({ searchParams }) {
+  const groupName = searchParams.groupName || "landingPage";
+
+  return {
     title: "Home | My Website",
     description: "Welcome to my homepage.",
-    url: "https://expense-tracker-alpha-lyart.vercel.app/",
-    type: "website",
-    images: [
-      {
-        url: "https://expense-tracker-alpha-lyart.vercel.app/api/og?groupName=landingPage",
-        width: 1200,
-        height: 630,
-      },
-    ],
-  },
-};
+    openGraph: {
+      title: "Home | My Website",
+      description: "Welcome to my homepage.",
+      url: "https://expense-tracker-alpha-lyart.vercel.app/",
+      type: "website",
+      images: [
+        {
+          url: `https://expense-tracker-alpha-lyart.vercel.app/api/og?groupName=${groupName}`,
+          width: 1200,
+          height: 630,
+        },
+      ],
+    },
+  };
+}
 
 export default function Home() {
   return (
