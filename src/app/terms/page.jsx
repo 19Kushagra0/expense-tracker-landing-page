6f456663a5625ac78af84ce10aacf44abf71b73e
19Kushagra0 <19Kushagra0@gmail.com>
2025-11-13 23:47:16 +0530

    copy page mistake solved

--- src/app/terms/page.jsx
+++ src/app/terms/page.jsx
@@ -12,7 +12,7 @@ export const metadata = {
       "Read the Terms of Service for My Website to understand your rights, responsibilities, and how we handle your data.",
     images: [
       {
-        url: "https://expense-tracker-alpha-lyart.vercel.app/api/og?groupName=termsPage", // ✅ CORRECT
+        url: "https://expense-tracker-alpha-lyart.vercel.app/api/og/termOg", // ✅ OG image you made
         width: 1200,
         height: 630,
       },
