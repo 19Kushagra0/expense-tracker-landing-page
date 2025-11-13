6e259e70a1ad0687b65a6f73e9d13e9caefeb288
19Kushagra0 <19Kushagra0@gmail.com>
2025-11-14 00:32:17 +0530

    Dev kushagra (#40)
    * navbar created
    
    * navbar added to landing page
    
    * Careers spelling mistake solved
    
    * style.css unnecessary comments and duplicate class definitions removed
    
    * style.css edited and auto scroll added
    
    * navbar design changed
    
    * demo career page created
    
    * career page connected and final bugs solved
    
    * created auto scrolling in privacy page
    
    * navbar edited
    
    changed navbar dark mode to light mode and box shadow removed
    
    * navbar edited
    
    * trying OG image
    
    * og image height and weight updated
    
    * dynamic og image testing
    
    * og image test 2
    
    * og location fixed
    
    * og image location changed
    
    * removing edge
    
    * Og test 3
    
    * og test 4
    
    * landPageOg edited
    
    * using image in dynamic og
    
    * bug fixed
    
    * dynamic og image edited
    
    * og image color darken
    
    * folder structure change for og image
    
    * term og page created
    
    * craeted privacy og image
    
    * trying to create differet link using og images
    
    * edited link in og/route
    
    * folder structure changed
    
    * copy page mistake solved
    
    * og change again
    
    * term page og structure changed
    
    * term og structure chaneg again

--- src/app/terms/page.jsx
+++ src/app/terms/page.jsx
@@ -1,24 +1,31 @@
-import React from "react";
 import Terms from "@/app/terms/components/Terms";
-import Header from "../myComponents/Header/Header";
 
-export const metadata = {
-  title: "Terms of Service | My Website",
-  description:
-    "Read the Terms of Service for My Website to understand your rights, responsibilities, and how we handle your data.",
-  openGraph: {
+export const dynamic = "force-dynamic"; // MUST for reading searchParams
+
+export function generateMetadata({ searchParams }) {
+  // Want Terms OG? Then lock it to termsPage
+  const groupName = searchParams.groupName || "termsPage";
+
+  return {
     title: "Terms of Service | My Website",
     description:
       "Read the Terms of Service for My Website to understand your rights, responsibilities, and how we handle your data.",
-    images: [
-      {
-        url: "https://expense-tracker-alpha-lyart.vercel.app/api/og/termOg", // ✅ OG image you made
-        width: 1200,
-        height: 630,
-      },
-    ],
-  },
-};
+    openGraph: {
+      title: "Terms of Service | My Website",
+      description:
+        "Read the Terms of Service for My Website to understand your rights, responsibilities, and how we handle your data.",
+      url: "https://expense-tracker-alpha-lyart.vercel.app/terms",
+      type: "website",
+      images: [
+        {
+          url: `https://expense-tracker-alpha-lyart.vercel.app/api/og?groupName=${groupName}`,
+          width: 1200,
+          height: 630,
+        },
+      ],
+    },
+  };
+}
 
 export default function page() {
   return (
