9087d56a99e9a355e8ed1db3724cebdd2d42440f
19Kushagra0 <19Kushagra0@gmail.com>
2025-11-14 17:36:53 +0530

    using parent instead of parameter

--- src/app/join-group/page.jsx
+++ src/app/join-group/page.jsx
@@ -3,8 +3,9 @@ import JoinGroup from "./components/Join-group";
 
 export const dynamic = "force-dynamic";
 
-export async function generateMetadata({ searchParams }) {
-  const groupName = searchParams?.groupName || "Your Group";
+export async function generateMetadata(_, parent) {
+  const url = new URL(parent.url);
+  const groupName = url.searchParams.get("groupName") || "Your Group";
 
   const baseUrl = "https://expense-tracker-alpha-lyart.vercel.app";
 
