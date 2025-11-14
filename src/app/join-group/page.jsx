083c65a568fe0c4b57b147ddec5696801f1f7567
19Kushagra0 <19Kushagra0@gmail.com>
2025-11-14 17:25:22 +0530

    link changed

--- src/app/join-group/page.jsx
+++ src/app/join-group/page.jsx
@@ -1,21 +1,31 @@
 import { Suspense } from "react";
 import JoinGroup from "./components/Join-group";
 
-export const dynamic = "force-dynamic"; // required for dynamic metadata
+export const dynamic = "force-dynamic";
 
 export async function generateMetadata({ searchParams }) {
   const groupName = searchParams?.groupName || "Your Group";
+
+  const baseUrl = "https://expense-tracker-alpha-lyart.vercel.app";
 
   return {
     title: `${groupName} | Numora`,
     openGraph: {
       title: `${groupName} | Numora`,
-      images: [`/api/og/join-group?groupName=${encodeURIComponent(groupName)}`],
+      images: [
+        `${baseUrl}/api/og/join-group?groupName=${encodeURIComponent(
+          groupName
+        )}`,
+      ],
     },
     twitter: {
       card: "summary_large_image",
       title: `${groupName} | Numora`,
-      images: [`/api/og/join-group?groupName=${encodeURIComponent(groupName)}`],
+      images: [
+        `${baseUrl}/api/og/join-group?groupName=${encodeURIComponent(
+          groupName
+        )}`,
+      ],
     },
   };
 }
