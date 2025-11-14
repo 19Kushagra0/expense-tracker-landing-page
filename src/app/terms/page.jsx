f8847ecc31e84c982cfc3436e6971cffc5e9b556
19Kushagra0 <19Kushagra0@gmail.com>
2025-11-15 04:32:57 +0530

    term page fixed

--- src/app/terms/page.jsx
+++ src/app/terms/page.jsx
@@ -1,14 +1,24 @@
-import JoinGroup from "./components/Join-group";
+import Terms from "@/app/terms/components/Terms";
+
+export const dynamic = "force-dynamic"; // MUST for reading searchParams
 
 export async function generateMetadata({ searchParams }) {
-  const params = await searchParams;
-  const groupName = params.groupName || "Your Group";
+  const params = await searchParams; // ⭐ REQUIRED FIX
+  const groupName = params.groupName || "termsPage";
 
   return {
+    title: "Terms of Service | My Website",
+    description:
+      "Read the Terms of Service for My Website to understand your rights, responsibilities, and how we handle your data.",
     openGraph: {
+      title: "Terms of Service | My Website",
+      description:
+        "Read the Terms of Service for My Website to understand your rights, responsibilities, and how we handle your data.",
+      url: "https://expense-tracker-alpha-lyart.vercel.app/terms",
+      type: "website",
       images: [
         {
-          url: `/api/og/join-group?groupName=${groupName}`,
+          url: `https://expense-tracker-alpha-lyart.vercel.app/api/og?groupName=${groupName}`,
           width: 1200,
           height: 630,
         },
@@ -17,12 +27,11 @@ export async function generateMetadata({ searchParams }) {
   };
 }
 
-export default async function Page({ searchParams }) {
-  const params = await searchParams; // ⬅️ FIX
-  const groupName = params.groupName || "Your Group";
-
-  return <JoinGroup groupName={groupName} />;
+export default function Page() {
+  return (
+    <div>
+      <Terms />
+    </div>
+  );
 }
-//
-//
 //
