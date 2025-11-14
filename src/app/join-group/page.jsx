3e7c3123f56fa1c9a42d9bb4f978428c5e2bfaeb
19Kushagra0 <19Kushagra0@gmail.com>
2025-11-15 04:30:39 +0530

    page changed

--- src/app/join-group/page.jsx
+++ src/app/join-group/page.jsx
@@ -1,5 +1,22 @@
 import JoinGroup from "./components/Join-group";
 
+export async function generateMetadata({ searchParams }) {
+  const params = await searchParams;
+  const groupName = params.groupName || "Your Group";
+
+  return {
+    openGraph: {
+      images: [
+        {
+          url: `/api/og/join-group?groupName=${groupName}`,
+          width: 1200,
+          height: 630,
+        },
+      ],
+    },
+  };
+}
+
 export default async function Page({ searchParams }) {
   const params = await searchParams; // ⬅️ FIX
   const groupName = params.groupName || "Your Group";
