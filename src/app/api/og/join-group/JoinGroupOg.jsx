28cfca5297761c37c43308a0c954fcae66452dd8
19Kushagra0 <19Kushagra0@gmail.com>
2025-11-16 06:52:47 +0530

    Dev kushagra (#90)
    * navbar created
    
    * navbar added to landing page
    
    * Careers spelling mistake solved
    
    * style.css unnecessary comments and duplicate class definitions removed
    
    * style.css edited and auto scroll added
    
    * navbar design changed
    
    * demo career page created
    
    * career page connected and final bugs solved
    
    * created auto scrolling in privacy page
    
    * navbar edited
    
    changed navbar dark mode to light mode and box shadow removed
    
    * navbar edited
    
    * trying OG image
    
    * og image height and weight updated
    
    * dynamic og image testing
    
    * og image test 2
    
    * og location fixed
    
    * og image location changed
    
    * removing edge
    
    * Og test 3
    
    * og test 4
    
    * landPageOg edited
    
    * using image in dynamic og
    
    * bug fixed
    
    * dynamic og image edited
    
    * og image color darken
    
    * folder structure change for og image
    
    * term og page created
    
    * craeted privacy og image
    
    * trying to create differet link using og images
    
    * edited link in og/route
    
    * folder structure changed
    
    * copy page mistake solved
    
    * og change again
    
    * term page og structure changed
    
    * term og structure chaneg again
    
    * privacy og image structure changed
    
    * created join option page
    
    * error solved
    
    * export const dynamic = "force-dynamic"; added to page.jsx
    
    * retry
    
    * deleting og image of join-group
    
    * problem solved
    
    * changed join group preview with parameter
    
    * changing file name
    
    * join-group code revised
    
    * import { ImageResponse } from "next/og"; import JoinGroupOg from "./JoinGroupOg";  export async function GET(req) {   const { searchParams } = new URL(req.url);   const groupName = searchParams.get("groupName") || "Join our group";    return new ImageResponse(<JoinGroupOg groupName={groupName} />, {     width: 1200,     height: 630,   }); }
    
    * changing layout
    
    * simplify code
    
    * join previews test
    
    * removing suspense from page.jsx
    
    * props instead of useSearchParams
    
    * PAGE CHANGED
    
    * change to back
    
    * error solved
    
    * deleting og image of join page
    
    * delete join og image
    
    * usinf suspense
    
    * craeted og of join page again
    
    * link changed
    
    * using parent instead of parameter
    
    * using header instead of parent
    
    * code reset
    
    * use header
    
    * using parameter again
    
    * layout edited
    
    * Update page.jsx
    
    * re try
    
    * change og code
    
    * dynamic og page fixed
    
    * again
    
    * page changed
    
    * term page fixed
    
    * preview detail added
    
    * og image edited
    
    * using link in dowlaod button
    
    * join-group css changed
    
    * background image updated
    
    * responsiveness updated
    
    * join page made unscrollable
    
    * scrollbar hidden
    
    * OG IMAGE UPDATED
    
    * og image edited
    
    ---------
    
    Co-authored-by: 19Kushagra0 <95228057+Kushagra0-19@users.noreply.github.com>

--- src/app/api/og/join-group/JoinGroupOg.jsx
+++ src/app/api/og/join-group/JoinGroupOg.jsx
@@ -4,8 +4,8 @@ export default function JoinGroupOg({ groupName }) {
   return (
     <div
       style={{
-        width: "1200px",
-        height: "630px",
+        width: "500px",
+        height: "500px",
         background: "white",
         display: "flex",
         flexDirection: "column",
@@ -36,9 +36,9 @@ export default function JoinGroupOg({ groupName }) {
           flexDirection: "row",
           alignItems: "center",
           justifyContent: "center",
-          marginTop: "30px",
+          marginTop: "90px",
           marginLeft: "30px",
-          fontSize: "16px",
+          fontSize: "10px",
           lineHeight: "normal",
           fontWeight: "400",
           letterSpacing: "normal",
@@ -50,11 +50,11 @@ export default function JoinGroupOg({ groupName }) {
 
       <div
         style={{
-          fontSize: "78px",
+          fontSize: "27px",
           fontWeight: 700,
           color: "#111",
-          marginTop: "250px",
-          marginLeft: "35px",
+          marginTop: "240px",
+          marginLeft: "10px",
         }}
       >
         {groupName}
@@ -62,14 +62,14 @@ export default function JoinGroupOg({ groupName }) {
 
       <div
         style={{
-          fontSize: 24,
+          fontSize: "15px",
           lineHeight: 26.1,
           fontWeight: 400,
           letterSpacing: 0, // "normal" becomes 0 in OG
           color: "#5f5f5f",
           fontFamily: "Inter",
-          marginTop: "-250px",
-          marginLeft: "35px",
+          marginTop: "-170px",
+          marginLeft: "10px",
         }}
       >
         Add, track and split expenses with your friends
@@ -82,13 +82,13 @@ export default function JoinGroupOg({ groupName }) {
         height={50}
         style={{
           objectFit: "contain",
-          width: "50px",
-          height: "50px",
+          width: "25px",
+          height: "25px",
           backgroundColor: "black",
           position: "absolute",
 
           right: "50px",
-          bottom: "150px",
+          bottom: "70px",
           backgroundColor: "transparent",
         }}
       />
