1d24e4d522a3990d24ae10a6887e950cde9fb8c7
19Kushagra0 <19Kushagra0@gmail.com>
2025-12-12 07:13:18 +0530

    Update LandingOg.jsx

--- src/app/api/og/landingOg/LandingOg.jsx
+++ src/app/api/og/landingOg/LandingOg.jsx
@@ -46,7 +46,7 @@ export default function LandingOg() {
 
       {/* RIGHT SECTION */}
       <img
-        src="https://expense-tracker-alpha-lyart.vercel.app/images/small.svg"
+        src="https://expense-tracker-landing-page-five.vercel.app/images/small.svg"
         alt="Hero Banner"
         width={277}
         height={415}
